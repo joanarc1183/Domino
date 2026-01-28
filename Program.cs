f676c9719f0ea312f49496262166a753b6d7df31
joanedna1183 <joanedna11@gmail.com>
2026-01-28 14:38:58 +0700

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,339 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DominoGame
+{
+    // ================= ENUMS =================
+    public enum Dot { Blank, One, Two, Three, Four, Five, Six }
+    public enum DominoOrientation { Horizontal, Vertical }
+    public enum BoardSide { Left, Right }
+
+    // ================= INTERFACES =================
+    public interface IDomino
+    {
+        Dot LeftPip { get; }
+        Dot RightPip { get; }
+        DominoOrientation Orientation { get; set; }
+        bool IsDouble();
+        bool CanConnect(IDomino other);
+    }
+
+    public interface IBoneyard
+    {
+        List<IDomino> Dominoes { get; }
+        IDomino Draw();
+        bool IsEmpty { get; }
+    }
+
+    public interface IBoard
+    {
+        LinkedList<IDomino> Dominoes { get; }
+        bool IsEmpty { get; }
+        Dot LeftEnd { get; }
+        Dot RightEnd { get; }
+        bool CanPlace(IDomino domino);
+        bool CanPlace(IDomino domino, BoardSide side);
+        void Place(IDomino domino, BoardSide side);
+        void Reset();
+    }
+
+    public interface IPlayer
+    {
+        int Id { get; }
+        string Name { get; }
+        int Score { get; set; }
+        List<IDomino> Hand { get; }
+    }
+
+    public interface IConsoleRenderer
+    {
+        void Start();
+    }
+
+    // ================= IMPLEMENTATIONS =================
+
+    public sealed class Domino : IDomino
+    {
+        public Dot LeftPip { get; private set; }
+        public Dot RightPip { get; private set; }
+        public DominoOrientation Orientation { get; set; }
+
+        public Domino(Dot left, Dot right)
+        {
+            LeftPip = left;
+            RightPip = right;
+            Orientation = DominoOrientation.Horizontal;
+        }
+
+        public bool IsDouble() => LeftPip == RightPip;
+
+        public bool CanConnect(IDomino other)
+        {
+            return LeftPip == other.LeftPip ||
+                   LeftPip == other.RightPip ||
+                   RightPip == other.LeftPip ||
+                   RightPip == other.RightPip;
+        }
+
+        public Domino Flip()
+        {
+            return new Domino(RightPip, LeftPip) { Orientation = Orientation };
+        }
+
+        public override string ToString() => $"[{LeftPip}|{RightPip}]";
+    }
+
+    public class Boneyard : IBoneyard
+    {
+        public List<IDomino> Dominoes { get; private set; } = new();
+        public bool IsEmpty => Dominoes.Count == 0;
+
+        public Boneyard(IEnumerable<IDomino> set)
+        {
+            Dominoes = set.ToList();
+            Shuffle();
+        }
+
+        public IDomino Draw()
+        {
+            if (IsEmpty) throw new InvalidOperationException("Boneyard empty");
+            var d = Dominoes[0];
+            Dominoes.RemoveAt(0);
+            return d;
+        }
+
+        private void Shuffle()
+        {
+            var rnd = new Random();
+            Dominoes = Dominoes.OrderBy(_ => rnd.Next()).ToList();
+        }
+    }
+
+    public class Board : IBoard
+    {
+        public LinkedList<IDomino> Dominoes { get; private set; } = new();
+        public bool IsEmpty => Dominoes.Count == 0;
+        public Dot LeftEnd => Dominoes.First!.Value.LeftPip;
+        public Dot RightEnd => Dominoes.Last!.Value.RightPip;
+
+        public bool CanPlace(IDomino domino)
+        {
+            if (IsEmpty) return true;
+            return CanPlace(domino, BoardSide.Left) || CanPlace(domino, BoardSide.Right);
+        }
+
+        public bool CanPlace(IDomino domino, BoardSide side)
+        {
+            if (IsEmpty) return true;
+            return side == BoardSide.Left
+                ? domino.LeftPip == LeftEnd || domino.RightPip == LeftEnd
+                : domino.LeftPip == RightEnd || domino.RightPip == RightEnd;
+        }
+
+        public void Place(IDomino domino, BoardSide side)
+        {
+            if (!CanPlace(domino, side))
+                throw new InvalidOperationException("Invalid placement");
+
+            if (IsEmpty)
+            {
+                Dominoes.AddFirst(domino);
+                return;
+            }
+
+            if (side == BoardSide.Left)
+            {
+                if (domino.RightPip == LeftEnd)
+                    Dominoes.AddFirst(domino);
+                else
+                    Dominoes.AddFirst(((Domino)domino).Flip());
+            }
+            else
+            {
+                if (domino.LeftPip == RightEnd)
+                    Dominoes.AddLast(domino);
+                else
+                    Dominoes.AddLast(((Domino)domino).Flip());
+            }
+        }
+
+        public void Reset() => Dominoes.Clear();
+    }
+
+    public class Player : IPlayer
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public int Score { get; set; }
+        public List<IDomino> Hand { get; } = new();
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+
+    // ================= GAME CONTROLLER (NO INTERFACE) =================
+
+    public class GameController
+    {
+        public event EventHandler? TurnStarted;
+        public event EventHandler? ActionExecuted;
+        public event EventHandler? RoundEnded;
+        public event EventHandler? GameEnded;
+
+        private readonly List<IPlayer> _players = new();
+        private readonly IBoard _board;
+        private IBoneyard _boneyard;
+        private int _currentPlayerIndex;
+        private int _consecutivePasses;
+        private bool _roundEnded;
+        private readonly int _maxScoreToWin;
+
+        public bool IsRoundEnded => _roundEnded;
+        public bool IsGameEnded { get; private set; }
+        public IPlayer? GameWinner { get; private set; }
+
+        public GameController(int maxScoreToWin)
+        {
+            _maxScoreToWin = maxScoreToWin;
+            _board = new Board();
+            _boneyard = new Boneyard(GenerateFullSet());
+        }
+
+        public void AddPlayer(IPlayer player) => _players.Add(player);
+
+        public IPlayer GetCurrentPlayer() => _players[_currentPlayerIndex];
+
+        public void StartRound()
+        {
+            ResetRound();
+            DealInitialHands();
+        }
+
+        public void PlayerAction()
+        {
+            if (_roundEnded) return;
+
+            var player = GetCurrentPlayer();
+            TurnStarted?.Invoke(player, EventArgs.Empty);
+
+            if (CanPlay(player))
+                HandlePlay(player);
+            else
+                HandlePass(player);
+
+            CheckRoundEnd();
+            MoveNext();
+        }
+
+        private void HandlePlay(IPlayer player)
+        {
+            var domino = player.Hand.First(d => _board.CanPlace(d));
+            var side = _board.CanPlace(domino, BoardSide.Left) ? BoardSide.Left : BoardSide.Right;
+
+            _board.Place(domino, side);
+            player.Hand.Remove(domino);
+            _consecutivePasses = 0;
+
+            ActionExecuted?.Invoke(player, EventArgs.Empty);
+        }
+
+        private void HandlePass(IPlayer player)
+        {
+            _consecutivePasses++;
+            if (!_boneyard.IsEmpty)
+                player.Hand.Add(_boneyard.Draw());
+        }
+
+        private void CheckRoundEnd()
+        {
+            var winner = _players.FirstOrDefault(p => p.Hand.Count == 0);
+            if (winner != null || _consecutivePasses >= _players.Count)
+            {
+                _roundEnded = true;
+                RoundEnded?.Invoke(winner, EventArgs.Empty);
+                UpdateScore(winner);
+                CheckGameEnd();
+            }
+        }
+
+        private void UpdateScore(IPlayer? winner)
+        {
+            if (winner == null) return;
+            winner.Score += _players.Where(p => p != winner)
+                .Sum(p => p.Hand.Sum(d => (int)d.LeftPip + (int)d.RightPip));
+        }
+
+        private void CheckGameEnd()
+        {
+            GameWinner = _players.FirstOrDefault(p => p.Score >= _maxScoreToWin);
+            if (GameWinner != null)
+            {
+                IsGameEnded = true;
+                GameEnded?.Invoke(GameWinner, EventArgs.Empty);
+            }
+        }
+
+        private void ResetRound()
+        {
+            _board.Reset();
+            _boneyard = new Boneyard(GenerateFullSet());
+            _currentPlayerIndex = 0;
+            _consecutivePasses = 0;
+            _roundEnded = false;
+            foreach (var p in _players) p.Hand.Clear();
+        }
+
+        private void DealInitialHands()
+        {
+            for (int i = 0; i < 7; i++)
+                foreach (var p in _players)
+                    p.Hand.Add(_boneyard.Draw());
+        }
+
+        private bool CanPlay(IPlayer player) => player.Hand.Any(d => _board.CanPlace(d));
+
+        private void MoveNext() => _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
+
+        private IEnumerable<IDomino> GenerateFullSet()
+        {
+            for (int i = 0; i <= 6; i++)
+                for (int j = i; j <= 6; j++)
+                    yield return new Domino((Dot)i, (Dot)j);
+        }
+    }
+
+    // ================= CONSOLE RENDERER =================
+
+    public class ConsoleRenderer : IConsoleRenderer
+    {
+        private readonly GameController _controller;
+
+        public ConsoleRenderer(GameController controller)
+        {
+            _controller = controller;
+            Subscribe();
+        }
+
+        public void Start()
+        {
+            while (!_controller.IsGameEnded)
+                _controller.PlayerAction();
+        }
+
+        private void Subscribe()
+        {
+            _controller.TurnStarted += (s, e) =>
+                Console.WriteLine($"Turn: {((IPlayer)s!).Name}");
+
+            _controller.RoundEnded += (s, e) =>
+                Console.WriteLine("Round Ended");
+
+            _controller.GameEnded += (s, e) =>
+                Console.WriteLine($"Winner: {((IPlayer)s!).Name}");
+        }
+    }
+}
